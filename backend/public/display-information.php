eb20585cd94182a49ece1c9bbc6028eb05524928
Ruben Simoes <ruben-r-simoes@telecom.pt>
2024-05-28 12:03:28 +0100

    Removed PHP Files from main branch

--- backend/public/display-information.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use SAPO\Jobs\Controllers\API\DisplayInformationController;
-
-require_once '/project/src/defines.php';
-
-$controller = new DisplayInformationController();
-$controller->execute();
